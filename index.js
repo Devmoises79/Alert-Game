27ff80278f8768c36d84b5ea633c381e735fb9d4
Devmoises79 <devmoisesss@gmail.com>
2025-01-25 00:47:58 -0300

    Guess Play

---
+++ index.js
@@ -0,0 +1,12 @@
+alert('Bem vindos ao jogo!');
+
+
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número de 0 e 30');
+
+
+if (numeroSecreto == chute) {
+    alert('Parabéns, você acertou!');
+} else {
+    alert('Você errou!');
+}
